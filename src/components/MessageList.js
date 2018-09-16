79b6ca029b1fab653331c15bf6b54796d3a3b500
shenfeng1945 <823902826@qq.com>
2018-09-16 20:26:42 +0800

    init project

---
+++ src/components/MessageList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MessageList extends Component {
+  render() {
+    return (
+      <div>
+        MessageList
+      </div>
+    )
+  }
+}
